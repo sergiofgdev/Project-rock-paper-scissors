92cf73b3f027c195c2cafd663b8950b43485cb6e
Sergio <98350551+sergiofgdev@users.noreply.github>
2022-11-29 10:40:07 +0100

    Creadas las funciones principales

--- game.js
+++ game.js
@@ -5,19 +5,48 @@
 // Paper = 1
 // Scissors = 2
 
-const choices = ["Rock","Paper", "Scissors"]
+const choices = ["Rock","Paper", "Scissors"];
+
+startGame();
+
+function startGame(){
+    computerSelection = getComputerChoice();
+    playerSelection =getPlayerChoice();
+    playRound(playerSelection, computerSelection);
+    
+}
 
 function getComputerChoice() {
     //code 
-    let computerNumber = Math.floor(Math.random() * 3)
-    console.log(choices[computerNumber])
+    let computerNumber = Math.floor(Math.random() * 3);
+    computerSelection = choices[computerNumber].toUpperCase();
+    console.log(computerSelection);
+    return computerSelection;
+    
+
+}
+
+//Eleccion jugador
+function getPlayerChoice(){
+    playerSelection = prompt("Elige una opcion").toUpperCase();
+    console.log(playerSelection);
+    return playerSelection;
+
 
 }
 
 // Juega una ronda 
 function playRound(playerSelection, computerSelection) {
     // code
+    let result;
+    if (playerSelection == computerSelection){
+        result = "Draw"
+    }else{
+        result = "Win or lose"
+    }
+    console.log(result)
+    return result;
 
 }
 
-getComputerChoice()
+
